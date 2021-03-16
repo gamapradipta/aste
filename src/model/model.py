65ab1c17877db2b461d90ba35ea2b2f64398b7c7
Gama Pradipta Wirawan <gamapradipta88@gmail.com>
2021-03-16 13:37:58 +0700

    Add initial ASTE model

---
+++ src/model/model.py
@@ -0,0 +1,81 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.callbacks import Callback
+from sklearn.metrics import f1_score
+from transformers import TFBertModel, BertTokenizer
+
+def into_tile(tensors, max_len):
+  tensors = tf.repeat(tf.expand_dims(tensors, axis=2), max_len, axis=2)
+  tensors_T = tf.transpose(tensors, perm=[0,2,1,3])
+  return tf.concat([tensors, tensors_T], axis=3)
+
+def triangular_mask(masks, max_len, num_class):
+  mask_a = tf.repeat(tf.expand_dims(masks, axis=1), max_len, axis=1)
+  mask_b = tf.repeat(tf.expand_dims(masks, axis=2), max_len, axis=2)
+  masks = mask_a * mask_b
+  return tf.repeat(tf.expand_dims(tf.linalg.band_part(masks, 0, -1), axis=3), num_class, axis=3)
+
+class MultiInferLayer(tf.keras.layers.Layer):
+  def __init__(self, args, **kwargs):
+    super().__init__(**kwargs)
+    self.args = args
+    self.feat_layer = tf.keras.layers.Dense(self.args.bert_feature_dim*2, name="linear_layer")
+    self.cls_layer = tf.keras.layers.Dense(self.args.class_num, activation = "softmax", name="cls_layer")
+  
+  def call(self, inputs, masks, tiled=True):
+    if not tiled:
+      inputs = into_tile(inputs, self.args.max_len)
+    return self._multi_hop(inputs, masks, self.args.nhop)
+
+  def _max_pooling(self, inputs):
+    inputs_a = tf.reduce_max(inputs, axis=1)
+    inputs_b = tf.reduce_max(inputs, axis=2)
+    inputs = tf.concat([tf.expand_dims(inputs_a, axis=3), tf.expand_dims(inputs_b, axis=3)], axis=3)
+    inputs = tf.reduce_max(inputs, axis=3)
+    return inputs
+
+  def _multi_hop(self, feat, masks, k):
+    masks = triangular_mask(masks, self.args.max_len, self.args.class_num)
+    logits = self.cls_layer(feat)
+    for i in range(k):
+      probs = logits
+      logits = probs * masks
+      logits = self._max_pooling(logits)
+      logits = into_tile(logits, self.args.max_len)
+      feat = tf.concat([feat, logits, probs], axis=3)
+      feat = self.feat_layer(feat)
+      logits = self.cls_layer(feat)
+    return logits
+
+class ASTE():
+  def __init__(self, args):
+    self.model = None
+    self.args = args
+
+  def init_model(self):
+    # BERT encoder
+    encoder_layer = TFBertModel.from_pretrained(self.args.bert_version)
+    # Finetuned or Feature Extraction
+    encoder_layer.trainable = self.args.fine_tuned
+    # GTS Model
+    input_ids = tf.keras.layers.Input(shape=(self.args.max_len,), dtype=tf.int32, name="Input_ID")
+    input_masks = tf.keras.layers.Input(shape=(self.args.max_len,), dtype=tf.float32, name="Masks")
+    mask = input_masks
+    feat = encoder_layer(input_ids, input_masks)[0]
+    # convert into tile
+    # feat = into_tile(feat, self.args.max_len)
+    logits = MultiInferLayer(self.args, name="MultiInfer_Layer")(feat, mask, False)
+    # logits = feat
+    self.model = tf.keras.Model(inputs=[input_ids, input_masks], outputs=[logits],)
+    optimizer = tf.keras.optimizers.Adam(lr=self.args.learning_rate)
+    self.model.compile(optimizer=optimizer, loss="categorical_cross_entropy", metrics=[""])
+  
+  def train(self, X_train, y_train, X_val=None, y_val=None, epochs=10, verbose=2, batch_size=64):
+    
+    self.model.fit(
+        X_train,
+        y_train,
+        epochs=epochs,
+        verbose=2,
+        batch_size=64
+    )
